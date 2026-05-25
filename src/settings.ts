72fc4e12e805696b6c69b2ef8ac4c2195fd71562
Jendrik <jendrik@bertram.eu>
2026-05-25 18:43:24 +0200

    feat: register plugin for homebridge 2

--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,4 @@
-/* eslint @typescript-eslint/no-var-requires: "off" */
-const { displayName, version } = require('../package.json');
-
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
 export const PLATFORM_NAME = 'knx-room';
-
-/**
- * This must match the name of your plugin as defined the package.json
- */
-export const PLUGIN_NAME = 'homebridge-' + PLATFORM_NAME;
-export const PLUGIN_DISPLAY_NAME = displayName;
-export const PLUGIN_VERSION = version;
+export const PLUGIN_NAME = '@jendrik/homebridge-knx-room';
+export const PLUGIN_DISPLAY_NAME = 'KNX Room';
+export const PLUGIN_VERSION = '0.1.2';
